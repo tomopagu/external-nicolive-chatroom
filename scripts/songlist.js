8e6c52268278f0996a6986dcdcc0d64973c54649
Thomas Moore <tsanm93@gmail.com>
2014-11-06 20:56:22 +0000

    When a new song is added, post a message

--- scripts/songlist.js
+++ scripts/songlist.js
@@ -1,5 +1,18 @@
 Songlist = new Mongo.Collection('songlist');
 
+Songlist.after.insert(function (userId, doc) {
+  var rankid = '<span class="rankid">'+doc.rankid+'</span>';
+  var name = '<span class="songname">'+doc.name+'</span>';
+  var producer = '<span class="producer">'+doc.producer+'</span>';
+  var nicolink = '<a href="http://www.nicovideo.jp/watch/'+doc.smcode+'" title="'+doc.name+'" target="_blank">'+doc.smcode+'</a>';
+
+  Messages.insert({
+    user: null,
+    content: rankid+': '+name+'  '+producer+' // '+nicolink,
+    createdAt: new Date() // current time
+  });
+});
+
 if (Meteor.isClient) {
   Meteor.subscribe("songlist");
   Template.songList.helpers({
