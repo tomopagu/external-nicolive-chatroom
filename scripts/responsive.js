7e980d05dada222ea7edf0e39466ab6f0f04c08a
Thomas Moore <tsanm93@gmail.com>
2014-11-15 18:05:47 +0000

    Most of #8 done, just got to limit the amount now

--- scripts/responsive.js
+++ scripts/responsive.js
@@ -6,6 +6,9 @@ if (Meteor.isClient) {
 
     var messagesAreaHeight = screenHeight - 60;
     $('.messages').height(messagesAreaHeight);
+    $('.message-list').height(messagesAreaHeight);
+
+    $('.messages').scrollTop( 9999 );
 
     var songsPosition = $('.songs').position();
     var songlistHeight = screenHeight - songsPosition.top;
